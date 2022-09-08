5102bb29af7c087bd037c120367d7331da5fab28
Olivia Gautheron <oxtramag@hotmail.fr>
2022-09-08 12:01:37 +0200

    first commit

---
+++ src/components/header/header.jsx
@@ -0,0 +1,15 @@
+import "./header.css";
+
+const Header = () => (
+    <>
+        <div className="header">
+            Je suis un Header !!!!
+            <div className=""> </div>
+        </div>
+    
+    
+    
+    </>
+    )
+
+export default Header;
---
+++ src/page/home/home.jsx
@@ -0,0 +1,14 @@
+import './home.css';
+
+const Home = () => (
+    <>
+        <div className="container">
+            <div className="container__center"> je suis un container</div>
+        </div>
+    
+    
+    
+    </>
+    )
+
+export default Home;
